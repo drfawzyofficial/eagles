2976c77f6905359d89c054bab6a7fc5c3ead0646
Your Name <you@example.com>
2020-12-30 22:42:52 +0200

    edit home

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -532,15 +532,27 @@ POTENZA.pieChart = function () {
   });
 })(jQuery);
 
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ef9e7664a7c6258179b8a7b/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
+
 // var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 // (function(){
 // var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
 // s1.async=true;
-// s1.src='https://embed.tawk.to/5ef9e7664a7c6258179b8a7b/default';
+// s1.src='https://embed.tawk.to/5fecdde9df060f156a926840/1eqqk5mau';
 // s1.charset='UTF-8';
 // s1.setAttribute('crossorigin','*');
 // s0.parentNode.insertBefore(s1,s0);
 // })();
+
 
 document.documentElement.className="js";var supportsCssVars=function(){
   var e,t=document.createElement("style");
